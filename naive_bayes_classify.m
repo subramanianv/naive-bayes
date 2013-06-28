03364489fdef1ef2991fbb7e02f327b91c7a9ece
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2013-06-27 23:20:19 -0400

    Update naive_bayes_classify.m

--- naive_bayes_classify.m
+++ naive_bayes_classify.m
@@ -3,7 +3,7 @@ function [ y ] = naive_bayes_classify(xv,pTrue,p0v,p1v)
 %   Detailed explanation goes here
 pfalse=1-pTrue;
 p1 = sum(xv.*p1v) + log(pTrue);
-p0 = sum(xv.*p0v) * log(pfalse);
+p0 = sum(xv.*p0v) + log(pfalse);
 if(p1 > p0)
     y=1;
 else 
